9076e2966bf9834b8aed48d04b4ba1fb9d2d9baa
Soham Banik <105418470+SohamB21@users.noreply.github.com>
2023-07-11 21:17:01 +0530

    Add files via upload

---
+++ JavaCollectionsFramework.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class JavaCollectionsFramework{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        
+        //ArrayList
+        ArrayList<Integer> aList = new ArrayList<Integer>();
+        //add, get, set, remove, size, isEmpty, contains, clear
+        aList.add(10);
+        aList.add(30);
+        aList.add(1, 20);
+        System.out.println(aList);
+        
+        //LinkedList
+    }
+}
