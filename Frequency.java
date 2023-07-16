87bf64f02eaaf83f50d06a6cc7c38e3bd3fd60c5
Soham Banik <105418470+SohamB21@users.noreply.github.com>
2023-07-17 00:14:09 +0530

    Update Frequency.java

--- Frequency.java
+++ Frequency.java
@@ -1,20 +1,42 @@
-import java.util.HashMap;
-import java.util.Map;
-class Frequency{
-    static int arr[] = {1, 2, 3, 1, 1, 4};
-    public static void main(String args[]){
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int i, n = arr.length;
-        for(i=0; i<n;i++){
-            if(map.containsKey(arr[i]))
-                map.put(arr[i], map.get(arr[i]) + 1);
-            else
-                map.put(arr[i], 1);
-        }
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            int num = entry.getKey();
-            int freq = entry.getValue();
-            
-        }
+import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        int arr[] = {10, 5, 10, 15, 10, 5};
+        int n = arr.length;
+        Frequency(arr, n);
     }
-}
+    static void Frequency(int arr[], int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        int maxFreq = 0, minFreq = n;
+        int maxEle = 0, minEle = 0;
+        // Traverse through map and find the elements
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int count = entry.getValue();
+            int element = entry.getKey();
+
+            if (count > maxFreq) {
+                maxEle = element;
+                maxFreq = count;
+            }
+            if (count < minFreq) {
+                minEle = element;
+                minFreq = count;
+            }
+        }
+
+        System.out.println("The highest frequency element is: " + maxEle);
+        System.out.println("The lowest frequency element is: " + minEle);
+    }
+} 
